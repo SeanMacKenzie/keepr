bfff599b08ee850babb34357c54e58400f95c7cd
Sean MacKenzie <sean.andrew.mackenzie@gmail.com>
2018-01-12 16:29:58 -0700

    vaultkeeprepo done. vaultkeepcont next

---
+++ server/Repositories/VaultkeepRespository.cs
@@ -0,0 +1,58 @@
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using keepr.Models;
+using Dapper;
+using MySql.Data.MySqlClient;
+
+namespace keepr.Repositories
+{
+    public class VaultkeepRepository : DbContext
+    {
+        public VaultkeepRepository(IDbConnection db) : base(db)
+        {
+        }
+
+        public IEnumerable<Vaultkeep> Getall()
+        {
+            return _db.Query<Vaultkeep>($"SELECT * FROM vaultkeeps");
+        }
+
+        public Vaultkeep GetById(int id)
+        {
+            return _db.QueryFirstOrDefault<Vaultkeep>($"SELECT * FROM vaultkeeps WHERE id = {id}");
+        }
+
+        public Vaultkeep Add(Vaultkeep vaultkeep)
+        {
+            int id = _db.ExecuteScalar<int>(@"INSERT INTO vaultkeeps (VaultId, KeepId, UserId)
+            VALUES (@VaultId, @KeepId, @UserId);
+            SELECT LAST_INSERT_ID()", new
+            {
+                vaultkeep.VaultId,
+                vaultkeep.KeepId,
+                vaultkeep.UserId
+            });
+            vaultkeep.Id = id;
+            return vaultkeep;
+        }
+
+        public Vaultkeep GetOneByIdAndUpdate(int id, Vaultkeep vaultkeep)
+        {
+            return _db.QueryFirstOrDefault<Vaultkeep>($@"
+                UPDATE vaultkeeps SET
+                    VaultId = @VaultId,
+                    KeepId = @KeepId,
+                    UserId = @UserId
+                WHERE Id = {id};
+                SELECT * FROM vaultkeeps WHERE id ={id};", vaultkeep);
+        }
+
+        public string RemoveById(int id)
+        {
+            var success = _db.Execute($"DELETE FROM vaultkeeps WHERE id = {id}");
+            return success > 0 ? "success" : "failed";
+        }
+    }
+}
